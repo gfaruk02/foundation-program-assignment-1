a31f22da8b9586b334f71085fccb1dce3f6d9585
gfaruk02 <gfaruk02@gmail.com>
2026-09-02 00:08:18 +0600

    Create if else logic for checking value

--- answer.js
+++ answer.js
@@ -1,12 +1,19 @@
 //Question No. 1 Value Detective
 
 function describeValue(value){
-return value;
+
+    if(value){
+        return `" ${typeof value} | truthy "`;
+    }else{
+        return `" ${typeof value} | falsy"`;
+    }
+// return ` " ${typeof value} | ${value? "truthy" : "falsy"} "`;
 };
-console.log(describeValue("hello"));	//"string | truthy"
+// console.log(describeValue("0"))
+// console.log(describeValue("hello"));	//"string | truthy"
 // console.log(describeValue(""));	//"string | falsy"
 // console.log(describeValue(25));	//"number | truthy"
-// console.log(describeValue(0));	//"number | falsy"
+console.log(describeValue(0));	//"number | falsy"
 // console.log(describeValue(true));	//"boolean | truthy"
 // console.log(describeValue(null));	//"object | falsy"
 // console.log(describeValue(undefined));
