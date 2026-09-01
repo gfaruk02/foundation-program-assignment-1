e0de61c6cbaf3de2cd2d66acfd6e5eb2ca76b771
gfaruk02 <gfaruk02@gmail.com>
2026-09-02 00:12:42 +0600

    check value NaN and 0

--- answer.js
+++ answer.js
@@ -7,13 +7,13 @@ function describeValue(value){
     }else{
         return `" ${typeof value} | falsy"`;
     }
-// return ` " ${typeof value} | ${value? "truthy" : "falsy"} "`;
 };
 // console.log(describeValue("0"))
+console.log(describeValue(NaN))
 // console.log(describeValue("hello"));	//"string | truthy"
 // console.log(describeValue(""));	//"string | falsy"
 // console.log(describeValue(25));	//"number | truthy"
-console.log(describeValue(0));	//"number | falsy"
+// console.log(describeValue(0));	//"number | falsy"
 // console.log(describeValue(true));	//"boolean | truthy"
 // console.log(describeValue(null));	//"object | falsy"
 // console.log(describeValue(undefined));
