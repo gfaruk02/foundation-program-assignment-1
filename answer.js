1248aa9d7f0bef54811f0da63ef43d1fc2652d85
gfaruk02 <gfaruk02@gmail.com>
2026-09-02 00:25:27 +0600

    Complete Q1 and start Q2

--- answer.js
+++ answer.js
@@ -1,18 +1,18 @@
 //Question No. 1 Value Detective
 
-function describeValue(value){
+// function describeValue(value){
 
-    // if(value){
-    //     return `"${typeof value} | truthy"`;
-    // }else{
-    //     return `"${typeof value} | falsy"`;
-    // }
+//     if(value){
+//         return `"${typeof value} | truthy"`;
+//     }else{
+//         return `"${typeof value} | falsy"`;
+//     }
 
-    //Ternary Operator
-    return `${typeof value} | ${value ? "truthy" : "falsy"}`;
+//     //Ternary Operator
+//     // return `${typeof value} | ${value ? "truthy" : "falsy"}`;
 
-};
-console.log(describeValue("0"));
+// };
+// console.log(describeValue("0"));
 // console.log(describeValue(NaN));
 // console.log(describeValue("hello"));
 // console.log(describeValue(""));
@@ -21,3 +21,7 @@ console.log(describeValue("0"));
 // console.log(describeValue(true));
 // console.log(describeValue(null));
 // console.log(describeValue(undefined));
+
+
+
+//Question No. 2 Bangladesh Weekend Machine
