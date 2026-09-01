ffd78b02cf80de1c0dcd386da2692f673170fbbc
gfaruk02 <gfaruk02@gmail.com>
2026-09-01 23:58:34 +0600

    Create function and set parametter value

--- answer.js
+++ answer.js
@@ -1,2 +1,12 @@
 //Question No. 1 Value Detective
-//
+
+function describeValue(value){
+return value;
+};
+console.log(describeValue("hello"));	//"string | truthy"
+// console.log(describeValue(""));	//"string | falsy"
+// console.log(describeValue(25));	//"number | truthy"
+// console.log(describeValue(0));	//"number | falsy"
+// console.log(describeValue(true));	//"boolean | truthy"
+// console.log(describeValue(null));	//"object | falsy"
+// console.log(describeValue(undefined));
